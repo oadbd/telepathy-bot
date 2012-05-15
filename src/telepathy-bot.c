73aca13c25a8c15336e1f46e653589a155f58c1b
Darryl Bidulock <oadbd@forkoff.org>
2012-05-15 06:40:20 -0600

    Create an account (if needed) and connect
    This commit is still a work in progress.
    
    A new local-xmpp account is created if none exist with the
    nickname defined in the BOTNAME environment variable.
    
    The presence of the account is set to AVAILABLE and then
    the bot will just sit there.

--- src/telepathy-bot.c
+++ src/telepathy-bot.c
@@ -1,92 +1,175 @@
-/*
- * telepathy-example-list-managers - list installed connection managers
- *
- * Copyright (C) 2007-2008 Collabora Ltd. <http://www.collabora.co.uk/>
- * Copyright (C) 2007-2008 Nokia Corporation
- *
- * Copying and distribution of this file, with or without modification,
- * are permitted in any medium without royalty provided the copyright
- * notice and this notice are preserved.
- */
-
 #include "config.h"
+#include "telepathy-bot.h"
 
-#include <telepathy-glib/telepathy-glib.h>
+static void 
+shutdown(BotRefs *bot)
+{
+	g_main_loop_quit (bot->loop);
 
-typedef struct {
-    GMainLoop *mainloop;
-    int exit_code;
-} ExampleData;
+	g_object_unref (bot->account_manager);
+	g_object_unref (bot->account);
+	g_main_loop_unref (bot->loop);
+}
 
 static void
-got_connection_managers (TpConnectionManager * const *cms,
-                         gsize n_cms,
-                         const GError *error,
-                         gpointer user_data,
-                         GObject *unused)
+account_online_cb (GObject *object,
+	GAsyncResult *res,
+	gpointer user_data)
 {
-  ExampleData *data = user_data;
-
-  if (error != NULL)
+	TpAccount *account = (TpAccount *) object;
+	BotRefs *bot = user_data;  
+	GError *error = NULL;
+	
+	if (!tp_account_request_presence_finish (account, res, &error))
     {
-      g_warning ("%s", error->message);
-      data->exit_code = 1;
+		g_print ("Error bringing account online: %s\n", error->message);
+		shutdown(bot);
+		return;
     }
-  else
-    {
-      TpConnectionManager * const *iter = cms;
-
-      g_message ("Found %" G_GSIZE_FORMAT " connection managers:", n_cms);
-
-      for (iter = cms; *iter != NULL; iter++)
-        {
-          gchar *name;
-
-          g_object_get (*iter,
-              "connection-manager", &name,
-              NULL);
-
-          g_message ("- %s", name);
-
-          g_free (name);
-        }
-    }
-
-  g_main_loop_quit (data->mainloop);
+    
+	g_print("ACCOUNT ONLINE\n");
 }
+
+static void
+connect_account (TpAccount *account, 
+	BotRefs *bot)
+{
+	g_print("In connect account.\n");
+	tp_account_request_presence_async(account,
+		TP_CONNECTION_PRESENCE_TYPE_AVAILABLE,
+		"ready to server you",
+		NULL,
+		account_online_cb,
+		bot);
+}
+
+static void
+account_created_cb (GObject *object,
+	GAsyncResult *res,
+	gpointer user_data)
+{
+	g_print("In account_created_cb.\n");
+	TpAccountManager *manager = (TpAccountManager *) object;
+	BotRefs *bot = user_data;  
+	GError *error = NULL;
+
+    TpAccount *account = tp_account_manager_create_account_finish(manager, res, &error);
+
+    if (error != NULL)
+    {
+		g_print ("Error creating account: %s\n", error->message);
+		shutdown(bot);
+		return;
+	}
+    g_print("Account created and ready.\n");
+
+	bot->account = g_object_ref(account);
+	connect_account(bot->account, bot);
+}
+
+static void
+account_manager_prepared_cb (GObject *object,
+    GAsyncResult *res,
+    gpointer user_data)
+{
+	g_print("In account_manager_prepared_cb\n");
+	TpAccountManager *manager = (TpAccountManager *) object;
+	BotRefs *bot = user_data;  
+	GList *accounts;
+
+	gchar *protocol;
+	gchar *name;
+	gchar normname[256];
+	
+	GError *error = NULL;
+
+
+	if (!tp_proxy_prepare_finish (object, res, &error))
+    {
+		g_print ("Error preparing AM: %s\n", error->message);
+		shutdown(bot);
+		return;
+    }
+  
+	TpAccount *account = NULL;
+	for (accounts = tp_account_manager_get_valid_accounts (manager);
+		accounts != NULL; accounts = g_list_delete_link (accounts, accounts))
+    {
+		account = accounts->data;
+		protocol = tp_account_get_protocol(account);
+		name = tp_account_get_nickname(account);
+      
+		if (protocol == "local-xmpp" && name == g_getenv("BOTNAME")) 
+		{
+			break;
+		}
+		account = NULL;
+	}
+
+	if (account == NULL)
+	{
+		g_print("Ready to create new account\n");
+		g_snprintf(&normname, sizeof(normname), "%s@%s", g_getenv("BOTNAME"), g_getenv("HOSTNAME"));
+		g_print("Creating new account\n");
+		tp_account_manager_create_account_async(manager,
+			"salut",
+			"local-xmpp",
+			"Telepathy BOT",
+			tp_asv_new(
+				"nickname", G_TYPE_STRING, g_getenv("BOTNAME"),
+				"first-name", G_TYPE_STRING, g_getenv("BOTNAME"),
+				"last-name", G_TYPE_STRING, g_getenv("BOTNAME"),
+			NULL),
+			tp_asv_new(
+				"Nickname", G_TYPE_STRING, g_getenv("BOTNAME"),
+				"NormalizedName", G_TYPE_STRING, &normname,
+			NULL),
+			account_created_cb,
+			bot
+		);
+		g_print("Waiting for account_created_cb\n");
+	}
+	else
+	{
+		g_print("Account already exists.\n");
+		bot->account = g_object_ref(account);
+		connect_account(bot->account, bot);
+    }
+}
+
+static TpAccountManager*
+prepare_manager(BotRefs *bot)
+{
+	g_print("In prepare_manager\n");
+  TpAccountManager *manager;
+  TpSimpleClientFactory *factory;
+
+  manager = tp_account_manager_dup ();
+  factory = tp_proxy_get_factory (manager);
+  tp_simple_client_factory_add_account_features_varargs (factory,
+      TP_ACCOUNT_FEATURE_CONNECTION,
+      0);
+  tp_simple_client_factory_add_connection_features_varargs (factory,
+      TP_CONNECTION_FEATURE_CONTACT_LIST,
+      0);
+  tp_simple_client_factory_add_contact_features_varargs (factory,
+      TP_CONTACT_FEATURE_ALIAS,
+      TP_CONTACT_FEATURE_CONTACT_GROUPS,
+      TP_CONTACT_FEATURE_INVALID);
+
+  tp_proxy_prepare_async (manager, NULL, account_manager_prepared_cb, bot);
+ 
+  return manager;
+}  
 
 int
 main (int argc,
       char **argv)
 {
-  ExampleData data = { g_main_loop_new (NULL, FALSE), 0 };
-  TpDBusDaemon *bus_daemon;
-  GError *error = NULL;
-
-  g_type_init ();
-  tp_debug_set_flags (g_getenv ("EXAMPLE_DEBUG"));
-
-  bus_daemon = tp_dbus_daemon_dup (&error);
-
-  if (bus_daemon == NULL)
-    {
-      g_warning ("%s", error->message);
-      g_error_free (error);
-      data.exit_code = 1;
-      goto out;
-    }
-
-  tp_list_connection_managers (bus_daemon, got_connection_managers, &data,
-      NULL, NULL);
-
-  g_main_loop_run (data.mainloop);
-
-out:
-  if (data.mainloop != NULL)
-    g_main_loop_unref (data.mainloop);
-
-  if (bus_daemon != NULL)
-    g_object_unref (bus_daemon);
-
-  return data.exit_code;
+	BotRefs bot;
+	g_type_init();
+	bot.loop = g_main_loop_new(NULL, FALSE);
+	bot.account_manager = prepare_manager(&bot);
+	g_main_loop_run(bot.loop);
+	return 0;
 }
